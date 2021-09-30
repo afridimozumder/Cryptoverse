171fc55d130b7d7d0fe762b542d930803bb463fa
Afridi Mozumder <49424469+afridimozumder@users.noreply.github.com>
2021-10-01 03:40:49 +0600

    Layout

--- src/App.js
+++ src/App.js
@@ -1,18 +1,48 @@
 import React from 'react';
-import { Switch, Route, Link } from 'react-router-dom';
+import { Switch, Route, Link, Router } from 'react-router-dom';
 import { Layout, Typography, Space } from 'antd';
+import { Navbar, Exchanges, Homepage, Cryptocurrencies, News, CryptoDetails } from './components';
+import './App.css';
 
 const App = () => {
     return (
         <div className='app'>
             <div className='navbar'>
-
+                <Navbar />
             </div>
             <div className='main'>
-
-            </div>
-            <div className='footer'>
-                
+                    <Layout>
+                        <div className='routes'>
+                            <Switch>
+                                <Route exact path='/'>
+                                    <Homepage />
+                                </Route>
+                                <Route exact path='/exchange'>
+                                    <Exchanges />
+                                </Route>
+                                <Route exact path='/cryptocurrencies'>
+                                    <Cryptocurrencies />
+                                </Route>
+                                <Route exact path='/crypto:coinId'>
+                                    <CryptoDetails />
+                                </Route>
+                                <Route exact path='/news'>
+                                    <News />
+                                </Route>
+                            </Switch>                        
+                        </div>                    
+                    </Layout>
+                <div className='footer'>
+                    <Typography.Title level={5} style={{color: 'white', textAlign: 'center'}}>
+                        Cryptoverse <br/>
+                        All rights reserved.
+                    </Typography.Title>
+                    <Space>
+                        <Link to='/'>Home</Link>
+                        <Link to='/exchanges'>Exchanges</Link>
+                        <Link to='/news'>News</Link>
+                    </Space>
+                </div>
             </div>
         </div>
     )
